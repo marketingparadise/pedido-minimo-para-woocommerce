d02f4d8dcb983ec4f829d70f65679153b3d5bbd4
Rafael Martin <rafael@mkparadise.com>
2025-12-05 14:04:45 +0100

    Añadir mensaje personalizado del usuario

--- includes/class-pedido-minimo-public.php
+++ includes/class-pedido-minimo-public.php
@@ -23,6 +23,7 @@ class PedidoMinimo_Public {
     public function woocommerce_importe_minimo() {
         $minimum = get_option( 'pedidominimo_precio_minimo', 0 );  // Value of the options page
         $incluir_descuentos = get_option( 'pedidominimo_incluir_descuentos', '0' ); // Value of the options page
+        $mensaje = get_option('pedidominimo_mensaje_pedido_minimo');
 
         if ($minimum <= 0 || ! WC()->cart ) {
             return;
@@ -33,10 +34,11 @@ class PedidoMinimo_Public {
             $subtotal_a_validar  = WC()->cart->get_subtotal();
         }
         if ( $subtotal_a_validar < $minimum ) {
-            /* translators: 1: minimum price value that the cart subtotal must reach, 2: the current cart subtotal. */
-            $mensaje = sprintf( __('You must place a <strong>minimum order of %1$s</strong> to complete your purchase. Your cart total is currently %2$s.', 'pedido-minimo-for-woocommerce') ,
-            wc_price( $minimum ),
-            wc_price( WC()->cart->subtotal )
+    
+            $mensaje = str_replace(
+                array( '{minimum}', '{total}' ),    // Placeholders
+                array( wc_price( $minimum ), wc_price( WC()->cart->subtotal )),
+                $mensaje
             );
             
             wc_add_notice( $mensaje, 'error' );
